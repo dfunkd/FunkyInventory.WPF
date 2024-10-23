885bc0b006133f16726420f96b4981461fd72e3f
Dylan Funk <dylanpfunk4@yahoo.com>
2024-10-23 08:48:09 -0500

    Initial Check-in

---
+++ Funky.Inventory.WPF/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Funky.Inventory.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            footer.Text = $"© {@DateTime.Now:yyyy} Funky Designs";
+        }
+    }
+}
