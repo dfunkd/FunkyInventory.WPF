5d5e1047f10063eccacdcc4d8f6367f8dc2a952f
Dylan Funk <dylanpfunk4@yahoo.com>
2024-10-28 14:35:57 -0500

    worked on data connection

---
+++ Funky.Inventory.WPF/Core/BaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Funky.Inventory.WPF.Core;
+
+public interface IContext
+{
+    string? ConnectionString { get; }
+    IDbConnection Connection { get; }
+}
+
+public abstract class BaseContext(string? connectionString) : IContext
+{
+    public string? ConnectionString { get; } = connectionString;
+    public virtual IDbConnection Connection => new SqlConnection(ConnectionString);
+}
